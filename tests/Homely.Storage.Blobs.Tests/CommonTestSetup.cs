2b1a6b9070527817be384bc957b3f646187427d9
Justin Adler <justin@homely.com.au>
2021-10-12 12:28:30 +1100

    Unit test fixes

--- tests/Homely.Storage.Blobs.Tests/CommonTestSetup.cs
+++ tests/Homely.Storage.Blobs.Tests/CommonTestSetup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,37 +8,47 @@ namespace Homely.Storage.Blobs.Tests
 {
     public abstract class CommonTestSetup
     {
-        protected string TestImageName = "2018-tesla-model-x-p100d.jpg";
-        protected string TestClassInstanceName = "elon-musk";
+        protected const string TestImageName = "2018-tesla-model-x-p100d.jpg";
+
         protected SomeFakeUser TestUser => new SomeFakeUser
         {
             Name = "Elon Musk",
             Age = 40
         };
 
-        protected async Task<AzureBlob> GetAzureBlobAsync(bool setupInitialBlobData = true)
+        protected async Task<(AzureBlob Blob, string ImageBlobId, string TestUserBlobId)> SetupAzureBlobAsync(bool setupInitialBlobData = true)
         {
             var logger = new NullLogger<AzureBlob>();
             var blob = new AzureBlob("UseDevelopmentStorage=true", "test-container", logger);
+
+            var imageBlobId = Guid.NewGuid().ToString();
+            var testUserBlobId = Guid.NewGuid().ToString();
 
             if (setupInitialBlobData)
             {
                 var cancellationToken = new CancellationToken();
 
                 // Do we have the image already?
-                if (await blob.GetAsync<string>(TestImageName, cancellationToken) == null)
+                if (await blob.GetAsync<string>(imageBlobId, cancellationToken) == null)
                 {
-                    var image = await File.ReadAllBytesAsync("2018-tesla-model-x-p100d.jpg");
-                    await blob.AddAsync(image, TestImageName);
+                    var image = await File.ReadAllBytesAsync(TestImageName);
+                    await blob.AddAsync(image, imageBlobId);
                 }
 
-                if (await blob.GetAsync<SomeFakeUser>(TestClassInstanceName, cancellationToken) == null)
+                if (await blob.GetAsync<SomeFakeUser>(testUserBlobId, cancellationToken) == null)
                 {
-                    await blob.AddAsync(TestUser, TestClassInstanceName);
+                    await blob.AddAsync(TestUser, testUserBlobId);
                 }
             }
 
-            return blob;
+            return (blob, imageBlobId, testUserBlobId);
+        }
+
+        protected async Task<AzureBlob> GetAzureBlobAsync(bool setupInitialBlobData = true)
+        {
+            var (azureBlob, _, _) = await SetupAzureBlobAsync(setupInitialBlobData);
+
+            return azureBlob;
         }
     }
 }
